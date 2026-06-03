565ed3587e0be302437cda41b410076660283866
Leonardo <vicentep.leo@gmail.com>
2026-06-03 18:34:57 -0400

    next

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@react-pdf/renderer'],
   turbopack: {
-    root: __dirname,
+    root: process.cwd(),
   },
 };
 
